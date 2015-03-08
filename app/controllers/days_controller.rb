d37c94b5e693ca3b193e7e6ee9d3c88a305377e7
Chris Tram <chris.tram@gmail.com>
2015-03-07 16:30:37 -0800

    User may edit multiple project codes and percentages at one time on the days#edit view.

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -1,15 +1,29 @@
 class DaysController < ApplicationController
   before_action :load_user_and_month
 
+  def show
+
+  end
+
   def edit
     # TODO: code days#edit view template to show an editable table of allocations
     # If day does not have alloactions, redirect to the day#new action
     # load a @allocations_array
     @allocations = @day.allocations
-
   end
+  def update
+    @allocations = []
+    params_proj_codes = params.select do |k,v|
+      k.include? 'project_code'
+    end
+    num_proj_codes = params_proj_codes.count / 2
 
-  def show
+    (0..num_proj_codes-1).each do |i|
+      @allocations << Allocation.create(project_code:params_proj_codes["project_code_#{i}_name"], percentage:params_proj_codes["project_code_#{i}_perc"], day_id: @day.id)
+      i += 1
+    end
+    @day.allocations = @allocations
+    redirect_to user_month_day_path(@user,@month,@day)
   end
 
   private
--- todo.rb
+++ todo.rb
@@ -1,1 +1,2 @@
-# TODO: the day show page should have a link to save the day -- until a user saves the day, modifications and additions to allocations should be held in MEMORY - in other words, when an allocation is added, it should not be said until the User clicks save on the Day page.
+# TODO: days#edit view has 10 sets of fields for entering project codes and percentages, instead of entering the project code and percentage pair one at a time.
+# TODO: User has default codes that auto-populate new days -- possibly the default codes are set each time a day is saved.
