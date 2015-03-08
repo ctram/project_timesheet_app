bfb26d2472e91139ea3a47bb9d33d9602c87237e
Chris Tram <chris.tram@gmail.com>
2015-03-07 19:33:45 -0800

    Troubleshoot forcing user to allocate 100% per day.

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -6,12 +6,16 @@ class DaysController < ApplicationController
   end
 
   def edit
+    binding.pry
     # TODO: code days#edit view template to show an editable table of allocations
     # If day does not have alloactions, redirect to the day#new action
     # load a @allocations_array
     @allocations = @day.allocations
   end
+
+  # FIXME: when using days#edit, params seems to be carrying over and adding more allocations than needed to the day object.
   def update
+    binding.pry
     @allocations = []
     params_proj_codes = params.select do |k,v|
       k.include? 'project_code'
@@ -20,11 +24,14 @@ class DaysController < ApplicationController
     (0..num_proj_codes-1).each do |i|
       @allocations << Allocation.create(project_code:params_proj_codes["project_code_#{i}_name"], percentage:params_proj_codes["project_code_#{i}_perc"], day_id: @day.id) unless params_proj_codes["project_code_#{i}_perc"] == ""
     end
-
-    binding.pry
-    @day.allocations = @allocations
-    redirect_to user_month_day_path(@user, @month, @day)
-
+    # TODO: allocations must total to 100% each day
+    if @day.sum_percentages != 0 and @day.sum_percentages != 100
+      flash[:error] = "Percentages allocated must equal 100%"
+      render :edit
+    else
+      @day.allocations = @allocations
+      redirect_to user_month_day_path(@user, @month, @day)
+    end
   end
 
   private
