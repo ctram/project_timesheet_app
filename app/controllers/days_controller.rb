87c967f2960e874f50c986d983f51c717e93f120
Chris Tram <chris.tram@gmail.com>
2015-03-07 18:28:03 -0800

    Mini refactor

--- app/controllers/days_controller.rb
+++ app/controllers/days_controller.rb
@@ -17,13 +17,14 @@ class DaysController < ApplicationController
       k.include? 'project_code'
     end
     num_proj_codes = params_proj_codes.count / 2
-
     (0..num_proj_codes-1).each do |i|
-      @allocations << Allocation.create(project_code:params_proj_codes["project_code_#{i}_name"], percentage:params_proj_codes["project_code_#{i}_perc"], day_id: @day.id)
-      i += 1
+      @allocations << Allocation.create(project_code:params_proj_codes["project_code_#{i}_name"], percentage:params_proj_codes["project_code_#{i}_perc"], day_id: @day.id) unless params_proj_codes["project_code_#{i}_perc"] == ""
     end
+
+    binding.pry
     @day.allocations = @allocations
-    redirect_to user_month_day_path(@user,@month,@day)
+    redirect_to user_month_day_path(@user, @month, @day)
+
   end
 
   private
--- todo.rb
+++ todo.rb
@@ -1,2 +1,1 @@
-# TODO: days#edit view has 10 sets of fields for entering project codes and percentages, instead of entering the project code and percentage pair one at a time.
 # TODO: User has default codes that auto-populate new days -- possibly the default codes are set each time a day is saved.
